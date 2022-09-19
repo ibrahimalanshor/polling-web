ba30f0c2ddf5990e53044e9c66cafe25ec646d8e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-09-19 21:55:49 +0700

    feat: add view poll page

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,23 +4,41 @@ export default [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/components/pages/HomePage.vue'),
+    component: () => import('@/components/pages/home-page.vue'),
     meta: {
       title: 'page.home.title',
     },
   },
   {
-    path: '/poll',
-    component: RouterView,
-    children: [
-      {
-        path: 'create',
-        name: 'PollCreate',
-        component: () => import('@/components/pages/poll/PollCreatePage.vue'),
-        meta: {
-          title: 'page.poll.page.create.title',
-        },
-      },
-    ],
+    path: '/new',
+    name: 'PollCreate',
+    component: () => import('@/components/pages/poll/poll-create.vue'),
+    meta: {
+      title: 'page.poll.page.create.title',
+    },
+  },
+  {
+    path: '/404',
+    name: 'NotFound',
+    component: () => import('@/components/pages/state/not-found-state.vue'),
+    meta: {
+      title: 'page.404.title',
+    },
+  },
+  {
+    path: '/error',
+    name: 'Error',
+    component: () => import('@/components/pages/state/error-state.vue'),
+    meta: {
+      title: 'page.400.title',
+    },
+  },
+  {
+    path: '/:poll',
+    name: 'PollView',
+    component: () => import('@/components/pages/poll/poll-view.vue'),
+    meta: {
+      title: 'page.poll.page.view.title',
+    },
   },
 ];
