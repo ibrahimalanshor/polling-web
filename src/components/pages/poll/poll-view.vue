45d5917eaa1452455d5600bdfc9b9d4d924ef055
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-09-20 06:44:14 +0700

    feat: add poll answer

--- src/components/pages/poll/poll-view.vue
+++ src/components/pages/poll/poll-view.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { HttpError } from '@/interfaces';
+import { reactive, ref } from 'vue';
 
 import { TemplateApp } from '@/components/templates';
 import { TemplatePollOptionListItem } from '@/components/templates/poll-option';
@@ -7,48 +7,91 @@ import { TemplatePollOptionListItem } from '@/components/templates/poll-option';
 import { useRouter, useRoute } from 'vue-router';
 import { useI18n } from 'vue-i18n';
 import { usePollFind } from '@/composes/poll';
+import { usePollAnswerCreate } from '@/composes/poll-answer';
 
 const router = useRouter();
 const route = useRoute();
 const { t } = useI18n();
-const { loading, poll, findPoll } = usePollFind();
+const { loading: loadingFindPoll, poll, findPoll } = usePollFind();
+const {
+  loading: loadingCreatePollAnswer,
+  body,
+  createPollAnswer,
+} = usePollAnswerCreate();
 
-const setPoll = async () => {
+const loadingPage = ref(true);
+const alert = reactive({
+  message: '',
+  color: 'error',
+  visible: false,
+});
+
+const setPoll = async () => await findPoll(route.params.poll);
+const setPage = async () => {
   try {
-    await findPoll(route.params.poll);
+    await setPoll();
 
     document.title = poll.value.name;
+
+    body.pollId = poll.value._id;
   } catch (err) {
     if (!(err instanceof HttpError)) {
       router.push({ name: 'Error' });
     } else {
       router.push({ name: 'NotFound' });
     }
+  } finally {
+    loadingPage.value = false;
   }
 };
 
-setPoll();
+const handleClickSave = async () => {
+  try {
+    await createPollAnswer();
+    await setPoll();
+
+    alert.message = t('page.poll.page.view.label.vote-success');
+    alert.color = 'success';
+    alert.visible = true;
+  } catch (err) {
+    alert.message = t('error.client');
+    alert.color = 'error';
+    alert.visible = true;
+  }
+};
+
+setPage();
 </script>
 
 <template>
   <template-app>
     <div class="my-24 max-w-screen-md mx-auto space-y-6">
-      <base-skeleton v-if="loading" />
+      <base-skeleton v-if="loadingPage" />
       <template v-else-if="poll">
         <h1 class="font-bold text-4xl">{{ poll.name }}</h1>
+        <base-alert
+          :color="alert.color"
+          :text="alert.message"
+          v-model="alert.visible"
+        />
         <div class="space-y-4">
           <template-poll-option-list-item
             v-for="pollOption in poll.options"
             :key="pollOption._id"
             :poll="poll"
             :poll-option="pollOption"
+            v-model="body.pollOptionId"
           />
         </div>
         <base-button
           :label="t('page.poll.page.view.action.save')"
           color="primary"
           size="lg"
+          :disabled="loadingFindPoll || loadingCreatePollAnswer"
+          :loading="loadingFindPoll || loadingCreatePollAnswer"
           block
+          v-on:click="handleClickSave"
+          v-if="body.pollOptionId"
         />
       </template>
     </div>
