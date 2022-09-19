418198cbb8fcaf6f5ca328e7fca42baf4f350ebf
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-09-19 21:51:31 +0700

    feat: add not found and error state page

---
+++ src/components/pages/state/not-found-state.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { TemplateApp } from '@/components/templates';
+
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
+
+<template>
+  <template-app>
+    <div class="my-24 max-w-2xl space-y-6">
+      <h1 class="font-bold text-4xl">{{ t('page.404.title') }}</h1>
+      <p class="text-lg">{{ t('page.404.description') }}</p>
+      <router-link :to="{ name: 'Home' }" custom v-slot="{ navigate }">
+        <base-button :label="t('page.404.action.back')" v-on:click="navigate" />
+      </router-link>
+    </div>
+  </template-app>
+</template>
